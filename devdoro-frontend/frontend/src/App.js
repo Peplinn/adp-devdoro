f09b4cfc5840e243f0cf05c9648655ea5a5824ca
andrilChioke <andril.chioke184@gmail.com>
2024-08-30 09:23:34 +0100

    Nothing done jsut moved the file out of the SIgn Up pages directory

---
+++ devdoro-frontend/frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import SignIn from './SIgn Up pages/SignIn';
+import SignUp from './SIgn Up pages/SignUp';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/" element={<SignIn />} /> {/* Default route to SignIn */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
